56681e7c410aa5af98f27d11a6575971a81d0964
zhenjiazhou <zhenjia.zhou@thoughtworks.com>
2024-03-03 21:09:40 +0800

    [UNC-AIM-70][Zhenjia Zhou] feat: create the user table

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # aim-ahead-api
+
+Change git username and password
+
+## local environment setup
+
+1. install python
+2. install pipenv
+3. colima
+
+`` pipenv install``˜
+
+
+## db migration
+
+flask db init  # Only needed the first time to create the migration repository
+flask db migrate -m "Create user table"
+flask db upgrade
---
+++ src/user/model/user.py
@@ -0,0 +1,14 @@
+from src.app import db
+from datetime import datetime
+
+class User(db.Model):
+    __tablename__ = 'user'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False, unique=True)
+    title = db.Column(db.String(128), nullable=True)
+    password = db.Column(db.String(192), nullable=False)
+    salt = db.Column(db.String(192), nullable=False)
+    admin_flag = db.Column(db.Boolean, default=False)
+    created_timestamp = db.Column(db.DateTime, default=datetime.utcnow)
