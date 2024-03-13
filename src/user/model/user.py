306f7656cb22b42d2a396d4e63fc9df852695b39
xinyang.li <xinyang.li@thoughtworks.com>
2024-03-13 14:50:45 +0800

    [UNCAIM-3][Xinyang] feat: add user files

--- src/user/model/user.py
+++ src/user/model/user.py
@@ -9,10 +9,13 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), nullable=False)
     email = db.Column(db.String(128), nullable=False, unique=True)
-    title = db.Column(db.String(128), nullable=True)
     password = db.Column(db.String(192), nullable=False)
     salt = db.Column(db.String(192), nullable=False)
     admin_flag = db.Column(db.Boolean, default=False)
+    position = db.Column(db.String(128), nullable=True)
+    employer = db.Column(db.String(128), nullable=True)
+    area_of_clinical_ex = db.Column(db.String(128), nullable=True)
+    active = db.Column(db.Boolean, default=False)
     created_timestamp = db.Column(db.DateTime, default=datetime.utcnow)
     modified_timestamp = db.Column(
         db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
