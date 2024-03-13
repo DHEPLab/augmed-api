dcfcdc8418a85608c61f71596a3174cea5700981
zhenjiazhou <zhenjia.zhou@thoughtworks.com>
2024-03-13 18:10:03 +0800

    [UNCAIM-88][Zhenjia Zhou] fix: fix commit conflict

--- src/user/repository/user_repository.py
+++ src/user/repository/user_repository.py
@@ -1,18 +1,23 @@
-from src import db
-from src.user.model.user import User
+from sqlalchemy import select
+
+from user.model.user import User
 
 
 class UserRepository:
-    @staticmethod
-    def create_user(user: User):
-        db.session.add(user)
-        db.session.commit()
+    def __init__(self, db):
+        self.db = db
+
+    def create_user(self, user: User):
+        self.db.session.add(user)
+        self.db.session.commit()
         return user
 
-    @staticmethod
-    def get_user_by_id(user_id):
-        return User.query.get(user_id)
+    def get_user_by_id(self, user_id):
+        statement = select(User).filter_by(id=user_id)
+        result = self.db.session.execute(statement).scalar_one_or_none()
+        return result
 
-    @staticmethod
-    def get_user_by_email(email):
-        return User.query.filter_by(email=email).first()
+    def get_user_by_email(self, email):
+        statement = select(User).filter_by(email=email)
+        result = self.db.session.execute(statement).scalar_one_or_none()
+        return result
