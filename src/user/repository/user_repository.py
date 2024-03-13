d87ee270673d23aba51cdcfb2610fef72e1d2276
zhenjiazhou <zhenjia.zhou@thoughtworks.com>
2024-03-13 15:18:09 +0800

    [UNCAIM-88][Zhenjia Zhou] feat: use user repository to query data instead of use db directly.

--- src/user/repository/user_repository.py
+++ src/user/repository/user_repository.py
@@ -12,3 +12,7 @@ class UserRepository:
     @staticmethod
     def get_user_by_id(user_id):
         return User.query.get(user_id)
+
+    @staticmethod
+    def get_user_by_email(email):
+        return User.query.filter_by(email=email).first()
--- src/user/service/auth_service.py
+++ src/user/service/auth_service.py
@@ -3,15 +3,15 @@ from werkzeug.security import check_password_hash
 
 from src.user.controller.request.loginRequest import LoginRequest
 from src.user.controller.response.loginResponse import LoginResponse
-from src.user.model.user import User
+from src.user.repository.user_repository import UserRepository
 
 
 class AuthService:
-    def __init__(self, database):
-        self.db = database
+    def __init__(self, user_repository: UserRepository):
+        self.user_repository = user_repository
 
     def login(self, login_request: LoginRequest) -> LoginResponse:
-        user = self.db.session.query(User).filter_by(email=login_request.email).first()
+        user = self.user_repository.get_user_by_email(login_request.email)
 
         if user and check_password_hash(user.password, login_request.password):
             access_token = create_access_token(identity=user.id, fresh=True)
