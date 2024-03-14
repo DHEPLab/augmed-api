ee36ebb626fd72eba0d1f80e034b9f5fa24f3b7c
zhenjiazhou <zhenjia.zhou@thoughtworks.com>
2024-03-14 15:12:19 +0800

    [UNCAIM-88][Zhenjia Zhou] fix: use api response for login response

--- src/user/controller/authController.py
+++ src/user/controller/authController.py
@@ -1,14 +1,14 @@
-import json
+from flask import Blueprint, request
 
-from flask import Blueprint, jsonify, request
-
+from common.model.ApiResponse import ApiResponse
+from src import db
 from src.user.controller.request.loginRequest import LoginRequest
 from src.user.repository.user_repository import UserRepository
 from src.user.service.auth_service import AuthService
 
 auth_blueprint = Blueprint("auth", __name__, url_prefix="/auth")
 
-user_repository = UserRepository()
+user_repository = UserRepository(db=db)
 
 auth_service = AuthService(user_repository=user_repository)
 
@@ -18,4 +18,6 @@ def login():
     req_data = request.get_json()
     login_request = LoginRequest(email=req_data["email"], password=req_data["password"])
     login_response = auth_service.login(login_request)
-    return jsonify(json.loads(login_response.to_json())), 200
+    return (
+        ApiResponse.success(login_response),
+        200)
