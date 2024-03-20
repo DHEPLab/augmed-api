b51ebc40b83ff36beaf8d9b2ff704332b156f84a
xinyang.li <xinyang.li@thoughtworks.com>
2024-03-20 16:24:07 +0800

    [UNCAIM-3][Xinyang] feat: add length

--- src/user/model/user.py
+++ src/user/model/user.py
@@ -13,9 +13,9 @@ class User(db.Model):
     password = db.Column(db.String(192), nullable=True)
     salt = db.Column(db.String(192), nullable=True)
     admin_flag = db.Column(db.Boolean, default=False)
-    position = db.Column(db.String(128), nullable=True)
-    employer = db.Column(db.String(128), nullable=True)
-    area_of_clinical_ex = db.Column(db.String(128), nullable=True)
+    position = db.Column(db.String(512), nullable=True)
+    employer = db.Column(db.String(512), nullable=True)
+    area_of_clinical_ex = db.Column(db.String(512), nullable=True)
     active = db.Column(db.Boolean, default=False)
     created_timestamp = db.Column(db.DateTime, default=datetime.utcnow)
     modified_timestamp = db.Column(
