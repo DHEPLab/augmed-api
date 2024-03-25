42e7fbf796e2db26b386c89335e6b4b81260fc90
TWDavidDai <weijian.dai@thoughtworks.com>
2024-03-25 17:20:16 +0800

    [UNCAIM-6][David] feat: verify user is invited & user is sign up & verify password

--- src/user/service/auth_service.py
+++ src/user/service/auth_service.py
@@ -1,8 +1,6 @@
 from datetime import datetime
 
 from flask_jwt_extended import create_access_token
-from werkzeug.exceptions import NotFound, Unauthorized
-from werkzeug.security import check_password_hash
 
 from src.common.exception.BusinessException import (BusinessException,
                                                     BusinessExceptionEnum)
@@ -11,7 +9,7 @@ from src.user.controller.request.loginRequest import LoginRequest
 from src.user.controller.request.signupRequest import SignupRequest
 from src.user.controller.response.loginResponse import LoginResponse
 from src.user.repository.user_repository import UserRepository
-from src.user.utils.pcrypt import generate_salt, pcrypt
+from src.user.utils.pcrypt import generate_salt, pcrypt, verify
 
 
 class AuthService:
@@ -22,10 +20,11 @@ class AuthService:
         user = self.user_repository.get_user_by_email(login_request.email)
 
         if not user:
-            raise NotFound("User not found")
-
-        if not check_password_hash(user.password, login_request.password):
-            raise Unauthorized("Invalid credentials")
+            raise BusinessException(BusinessExceptionEnum.UserNotInPilot)
+        if not user.active:
+            raise BusinessException(BusinessExceptionEnum.UserEmailIsNotSignup)
+        if not verify(login_request.password, user.salt, user.password):
+            raise BusinessException(BusinessExceptionEnum.UserPasswordIncorrect)
 
         additional_claims = {"last_login_time": datetime.now().isoformat()}
         access_token = create_access_token(
