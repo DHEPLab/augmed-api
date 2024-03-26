0a59d3c58e0907e10bbf956a4110c68f5121bbff
TWDavidDai <weijian.dai@thoughtworks.com>
2024-03-26 11:36:20 +0800

    [UNCAIM-6][David] test: add tests for login exception

--- tests/user/service/auth_service_test.py
+++ tests/user/service/auth_service_test.py
@@ -2,12 +2,12 @@ import re
 import pytest
 from src.common.exception.BusinessException import BusinessException, BusinessExceptionEnum
 from src.user.controller.request.signupRequest import SignupRequest
-from werkzeug.security import generate_password_hash
 
 from src.user.controller.request.loginRequest import LoginRequest
 from src.user.model.user import User
 from src.user.repository.user_repository import UserRepository
 from src.user.service.auth_service import AuthService
+from src.user.utils.pcrypt import pcrypt
 
 
 @pytest.fixture
@@ -16,8 +16,9 @@ def user():
         id=1,
         name='Test User',
         email='test@example.com',
-        password=generate_password_hash('password123'),
-        salt='somesalt'
+        password=pcrypt('password123', salt='somesalt'),
+        salt='somesalt',
+        active=True
     )
 
 
@@ -28,9 +29,11 @@ def user_repository_mock(mocker, user):
     mock_repo.query_user_by_email.return_value = None
     return mock_repo
 
+
 @pytest.fixture
 def invalid_singup_request():
     return SignupRequest("john@example.com", "simple password")
+
 
 @pytest.fixture
 def valid_singup_request():
@@ -42,29 +45,33 @@ def test_login_success(user_repository_mock, app, user):
         auth_service = AuthService(user_repository_mock)
         login_request = LoginRequest(email=user.email, password='password123')
         login_response = auth_service.login(login_request)
-
     assert login_response.access_token is not None
 
 
-def test_login_failure_with_wrong_email(user_repository_mock, app):
+def test_login_failure_with_not_invited_email(user_repository_mock, app):
     with app.app_context():
         auth_service = AuthService(user_repository_mock)
         user_repository_mock.get_user_by_email.return_value = None
         wrong_email_login_request = LoginRequest(email='wrong@example.com', password='password123')
-        with pytest.raises(Exception) as exc_info:
+        with pytest.raises(BusinessException, match=re.compile(BusinessExceptionEnum.UserNotInPilot.name)):
             auth_service.login(wrong_email_login_request)
 
-    assert 'User not found' in str(exc_info.value)
+
+def test_login_failure_with_not_sign_up_email(user_repository_mock, app, user):
+    with app.app_context():
+        auth_service = AuthService(user_repository_mock)
+        user_repository_mock.get_user_by_email.return_value = user.copy(active=False)
+        email_not_sign_up_login_request = LoginRequest(email=user.email, password='password123')
+        with pytest.raises(BusinessException, match=re.compile(BusinessExceptionEnum.UserEmailIsNotSignup.name)):
+            auth_service.login(email_not_sign_up_login_request)
 
 
 def test_login_failure_with_wrong_password(user_repository_mock, app, user):
     with app.app_context():
         auth_service = AuthService(user_repository_mock)
         login_request = LoginRequest(email=user.email, password='password1234')
-        with pytest.raises(Exception) as exc_info:
+        with pytest.raises(BusinessException, match=re.compile(BusinessExceptionEnum.UserPasswordIncorrect.name)):
             auth_service.login(login_request)
-
-    assert 'Invalid credentials' in str(exc_info.value)
 
 
 def test_signup_should_failed_when_user_password_is_invalid(user_repository_mock, invalid_singup_request):
@@ -76,7 +83,7 @@ def test_signup_should_failed_when_user_password_is_invalid(user_repository_mock
 
 def test_signup_should_failed_when_user_not_in_pilot(user_repository_mock, valid_singup_request):
     auth_service = AuthService(user_repository_mock)
-    
+
     with pytest.raises(BusinessException, match=re.compile(BusinessExceptionEnum.UserNotInPilot.name)):
         auth_service.signup(valid_singup_request)
 
@@ -84,6 +91,6 @@ def test_signup_should_failed_when_user_not_in_pilot(user_repository_mock, valid
 def test_signup_should_failed_when_user_already_signup(user, user_repository_mock, valid_singup_request):
     auth_service = AuthService(user_repository_mock)
     user_repository_mock.query_user_by_email.return_value = user.copy(active=True)
-    
+
     with pytest.raises(BusinessException, match=re.compile(BusinessExceptionEnum.UserEmailIsAlreadySignup.name)):
         auth_service.signup(valid_singup_request)
