a8799974bdc630cc9a1a227de2c739683513d94b
zhenjiazhou <116146901+zhenjiazhou@users.noreply.github.com>
2024-03-28 17:48:36 +0800

    [UNCAIM-88][Zhenjia Zhou]chore: change default access token expire time for test

--- src/config.py
+++ src/config.py
@@ -12,8 +12,8 @@ class Config:
     # JWT configuration
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
     JWT_ACCESS_TOKEN_EXPIRES = int(
-        os.getenv("JWT_ACCESS_TOKEN_EXPIRES", 15 * 60)
+        os.getenv("JWT_ACCESS_TOKEN_EXPIRES", 15)
     )  # Defaults to 15 minutes
     JWT_REFRESH_TOKEN_EXPIRES = int(
-        os.getenv("JWT_REFRESH_TOKEN_EXPIRES", 3 * 24 * 60 * 60)
+        os.getenv("JWT_REFRESH_TOKEN_EXPIRES", 2 * 60)
     )  # Defaults to 3 days
