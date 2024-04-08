14b21d117b957b3c30ae346bc05c2c2d714ae661
TWDavidDai <weijian.dai@thoughtworks.com>
2024-04-08 16:37:39 +0800

    [UNCAIM-99][David] feat: add mock case list api

--- src/user/controller/auth_controller.py
+++ src/user/controller/auth_controller.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, Response, json, request
+from flask import Blueprint, Response, json, request, jsonify
 
 from src import db
 from src.common.model.ApiResponse import ApiResponse
@@ -6,6 +6,7 @@ from src.user.controller.request.loginRequest import LoginRequest
 from src.user.controller.request.signupRequest import SignupRequest
 from src.user.repository.user_repository import UserRepository
 from src.user.service.auth_service import AuthService
+from src.user.utils.auth_utils import jwt_validation_required
 
 auth_blueprint = Blueprint("auth", __name__)
 
@@ -40,3 +41,42 @@ def signup() -> Response:
     response.status_code = 201
 
     return response
+
+@auth_blueprint.route("/case", methods=["GET"])
+@jwt_validation_required()
+def get_case():
+    return (
+        jsonify([
+            {
+                "id": "003-v112022",
+                "patient_chief_complaint": "Chest Pain",
+                "age": 52,
+                "sex": "Male"
+            },
+            {
+                "id": "001-v103979",
+                "patient_chief_complaint": "Polyuria",
+                "age": 63,
+                "sex": "Female"
+            },
+            {
+                "id": "003-v112052",
+                "patient_chief_complaint": "Chest Pain",
+                "age": 44,
+                "sex": "Male"
+            },
+            {
+                "id": "003-v112082",
+                "patient_chief_complaint": "Chest Pain",
+                "age": 33,
+                "sex": "Female"
+            },
+            {
+                "id": "003-v112092",
+                "patient_chief_complaint": "Chest Pain",
+                "age": 22,
+                "sex": "Male"
+            }
+        ]
+        ),
+        200,)
