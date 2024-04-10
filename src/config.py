e5a6c40da3fa5f057cf88b7131ead6ebe1ec82c4
haoran.li <haoran.li1@thoughtworks.com>
2024-04-10 16:32:36 +0800

    [UNCAIM-111][haoran.li]fix: update access token expires

--- src/config.py
+++ src/config.py
@@ -5,6 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()  # Load environment variables from .env file
 
 
+def days_to_seconds(days: int):
+    seconds_in_a_day = 24 * 60 * 60
+    return days * seconds_in_a_day
+
+
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -12,7 +17,7 @@ class Config:
     # JWT configuration
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
     JWT_ACCESS_TOKEN_EXPIRES = int(
-        os.getenv("JWT_ACCESS_TOKEN_EXPIRES", 15)
+        os.getenv("JWT_ACCESS_TOKEN_EXPIRES", days_to_seconds(3))
     )  # Defaults to 15 minutes
     JWT_REFRESH_TOKEN_EXPIRES = int(
         os.getenv("JWT_REFRESH_TOKEN_EXPIRES", 2 * 60)
